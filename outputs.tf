bacbd9b166b972c34d2f4d3b39ed031ab8a4ac61
Kilian <kilianc0709@gmail.com>
2024-03-08 10:44:07 +0100

    refa: added try function to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "id" {
-  description = "ID of the S3 bucket."
-  value       = aws_s3_bucket.main.id
+  description = "The ID of the S3 bucket."
+  value       = try(aws_s3_bucket.main.id, null)
 }
 
 output "arn" {
-  description = "ARN of the S3 bucket."
-  value       = aws_s3_bucket.main.arn
+  description = "The ARN of the S3 bucket."
+  value       = try(aws_s3_bucket.main.arn, null)
 }
 
 output "queues" {
   description = "List of objects of each created queue."
-  value = [for index, value in var.queues : {
+  value = [for index, value in try(var.queues, []) : {
     url = aws_sqs_queue.main[index].url
     arn = aws_sqs_queue.main[index].arn
   }]
